6a13a3581bfc49e46b5b982d10853ba312407c35
Nuno Neves <youknownuno@gmail.com>
2018-04-14 12:01:23 -0700

    created app component, and map component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-console.log('My Minimal React Webpack Babel Setup');
+console.log('Green like american money - Power Brokers');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const Map = (props) => {
+	return (
+		<div className="map">
+			<img src={props.src} alt="map"/>
+		</div>
+	);
+
+};
+
+class App extends React.Component {
+	constructor(props) {
+  super(props);
+
+  this.state = {
+    mapSource : "https://maps.googleapis.com/maps/api/staticmap?center=37.803826699999995,-122.27087429999999&zoom=13&size=300x300&sensor=false"
+  	};
+	}
+
+	render() {
+
+		return (
+			<div>
+			<Map src={this.state.mapSource}/>
+		</div>
+		);
+		
+	};
+}
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App/ >,
   document.getElementById('app')
 );
 

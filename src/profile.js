eeee537223aabc22d026f791492c164ca17e391f
Nuno Neves <youknownuno@gmail.com>
2018-04-17 10:57:39 -0700

    added slider functionality

--- src/profile.js
+++ src/profile.js
@@ -2,24 +2,34 @@ import React from 'react';
 
 class Profile extends React.Component {
 	constructor(props) {
-  super(props);
+	  super(props);
 
+	  this.state = {
+	  	user: "Thor",
+	  	src: "https://drive.google.com/uc?id=1YSq1kT4ocmBlLyJGdRM7mBoejMgJf_Q9",
+	  	price: 10,
+	  	mode: "$$$",
+	  }
+
+	  this.slide = function(val) {
+  		this.setState({price: val});
+  	}
 	}
 
 	render() {
 
 		return (
 			<div className="centered">
-				<img className="portrait" src="https://drive.google.com/uc?id=1O1JHkbb_IUCx2tmg2bDQsct6iQIb-VFV" height="250px" width="250px"/>
-				<b className="title" >Rowan Atkins</b>
-				<p className="transaction-watts" >Minimum selling price: $0.10</p>
-				<p className="transaction-watts" >Maximum buying price: $0.15</p>
+				<img className="portrait" src={this.state.src} height="250px" width="250px"/>
+				<b className="title" >{this.state.user}</b>
 
-        <input type="range" min="1" max="2" className="slider" id="slider"/>
+				<p className="transaction-watts" >$0.{this.state.price} per kwh</p>
+
+        <input type="range" min="8" max="20" className="slider" id="slider" onInput={(event) => {this.slide(event.target.value)}}/>
 
 				<div className="space-between">
 					<div className="medium">eco</div>
-					<div className="medium highlighted">$$$</div>
+					<div className="medium">$$$</div>
 				</div>
 				
       </div>

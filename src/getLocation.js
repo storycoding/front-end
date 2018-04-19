97ba3620bc161aaafa1488c45b489682d4fa099f
Nuno Neves <youknownuno@gmail.com>
2018-04-19 15:39:38 -0700

    changed NG_mylocation storage variable to PB_Location

--- src/getLocation.js
+++ src/getLocation.js
@@ -1,6 +1,6 @@
 const getLocation = function(callback) {
 
-  let storedLocation = JSON.parse(sessionStorage.getItem('NG_myLocation'));
+  let storedLocation = JSON.parse(sessionStorage.getItem('PB_Location'));
 
   if(storedLocation) {
     callback(storedLocation);
@@ -11,7 +11,7 @@ const getLocation = function(callback) {
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
     
-    sessionStorage.setItem('NG_myLocation', JSON.stringify({lat:latitude,lng: longitude}));
+    sessionStorage.setItem('PB_Location', JSON.stringify({lat:latitude,lng: longitude}));
     console.log(` success: { lat: ${latitude}, lon: ${longitude}}`);
     callback({lat: latitude, lng: longitude});
   }
